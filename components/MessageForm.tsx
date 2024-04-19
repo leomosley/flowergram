5d267d0b3ba08ca9b1d86cb4138970d07b35b17d
leomosley <leo.mosley2@gmail.com>
2024-04-19 12:28:43 +0100

    updated base url

--- components/MessageForm.tsx
+++ components/MessageForm.tsx
@@ -16,8 +16,8 @@ export default function MessageForm() {
   const generate = () => {
     setLoading(true);
     let url = `${
-      process.env.NEXT_PUBLIC_VERCEL_URL 
-      ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
+      process.env.VERCEL_URL 
+      ? 'https://' + process.env.VERCEL_URL
       : 'http://localhost:3000'
     }/message?`;
     

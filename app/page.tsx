6775f3a261d259ab56625f866cbd730a7a594e89
leomosley <leo.mosley2@gmail.com>
2024-04-29 17:20:29 +0100

    added responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <section className="flex justify-center mt-20">
       <div className="text-balance text-center mt-14 md:mt-28">
-        <h1 className="text-7xl font-extrabold glow">FlowerGram</h1>
-        <span className="flex text-lg glow max-w-md md:max-w-lg text-wrap">Send personalized digital flowers and messages to anyone, anywhere, absolutely free.</span>
+        <h1 className="text-5xlmd:text-7xl font-extrabold glow">FlowerGram</h1>
+        <span className="flex text-base md:text-lg glow max-w-md md:max-w-lg text-wrap">Send personalised digital flowers and messages to anyone, anywhere, absolutely free.</span>
       </div>
     </section>
   );

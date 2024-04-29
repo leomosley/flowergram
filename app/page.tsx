9e97e2e4f3d93e790eb0be83b3ea0fe3d8366fcd
leomosley <leo.mosley2@gmail.com>
2024-04-29 17:21:53 +0100

    fixed tpyo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="flex justify-center mt-20">
       <div className="text-balance text-center mt-14 md:mt-28">
-        <h1 className="text-5xlmd:text-7xl font-extrabold glow">FlowerGram</h1>
+        <h1 className="text-5xl md:text-7xl font-extrabold glow">FlowerGram</h1>
         <span className="flex text-base md:text-lg glow max-w-md md:max-w-lg text-wrap">Send personalised digital flowers and messages to anyone, anywhere, absolutely free.</span>
       </div>
     </section>

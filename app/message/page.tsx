f1217d8afe66f86b7726962550ec110a09b22376
leomosley <leo.mosley2@gmail.com>
2024-05-06 19:06:56 +0100

    added temp message

--- app/message/page.tsx
+++ app/message/page.tsx
@@ -1,8 +1,8 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-import Message from '@/components/Message';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { colours, Flower, flowers } from '@/flowers/index';
+import clsx from 'clsx';
 
 export default function MessagePage() {
   const searchParams = useSearchParams();
@@ -14,22 +14,48 @@ export default function MessagePage() {
   
   const [flower, setFlower] = useState<Flower | undefined>();
   const [colour, setColour] = useState<string>("");
-  
+    
   useEffect(() => {
     setFlower(flowers[Number(flowerParam)]);
     setColour(colours[Number(colourParam)]);
   }, [flowerParam, colourParam]);
 
+  useLayoutEffect(() => {
+    document.body.style.overflowY = 'hidden';
+  }, [searchParams]);
+
   return (
     <section className="absolute inset-0 flex items-end justify-center min-h-dvh">
-      {/* <Message 
-        message={messageParam}
-        flower={flower}
-        colour={colour}
-        sender={senderParam}
-        recipient={recipientParam}
-        flowers={flowers}
-      /> */}
+      {recipientParam && (
+        <div 
+          className={clsx(
+            "absolute top-1/4 -mt-20 left-1/4 p-2 rounded-xl border",
+            "backdrop-blur-xl glow -rotate-12"
+          )}
+        >
+          To: {recipientParam}
+        </div>
+      )}
+      {senderParam && (
+        <div 
+          className={clsx(
+            "absolute top-1/4 -mt-20 right-1/4 p-2 rounded-xl border",
+            "backdrop-blur-xl glow rotate-12"
+          )}
+        >
+          From: {senderParam}
+        </div>
+      )}
+      {messageParam && (
+        <div 
+          className={clsx(
+            "absolute flex justify-center bottom-1/4 mx-auto p-2 rounded-xl border z-50",
+            "backdrop-blur-xl glow"
+          )}
+        >
+          {messageParam}
+        </div>
+      )}
       {flower && flower.component}
     </section>
   );

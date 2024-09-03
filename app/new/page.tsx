e4c9b7c6cdb8f29b69f86dfa998d6ffb9620f600
leomosley <leo.mosley2@gmail.com>
2024-09-03 12:51:22 +0100

    fix:(new): add suspense boundary

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,9 +1,12 @@
 import { MessageForm } from '@/components/new/message-form';
+import { Suspense } from 'react';
 
 export default function New() {
   return (
     <section className="flex h-full pb-12 justify-center">
-      <MessageForm />
+      <Suspense fallback={<p>loading...</p>}>
+        <MessageForm />
+      </Suspense>
     </section>
   )
 }

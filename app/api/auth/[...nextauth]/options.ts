87efbceccfe0c0db6c58ceb8c18a51446dc45e4b
leomosley <leo.mosley2@gmail.com>
2024-10-16 20:47:07 +0100

    feat: add next-auth + prisma

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,40 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user = {
+          ...session.user,
+          id: token.id,
+          email: token.email,
+        };
+      }
+      return session;
+    },
+  },
+};
---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import { withAuth } from "next-auth/middleware";
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export default withAuth({
+  pages: {
+    signIn: "/sign-in",
+  },
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token && !(request.nextUrl.pathname !== "/" || request.nextUrl.pathname.startsWith("/sign-in"))) {
+    return NextResponse.redirect(new URL("/sign-in", request.nextUrl));
+  }
+
+  return NextResponse.next();
+} 

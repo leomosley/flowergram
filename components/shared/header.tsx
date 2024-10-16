df7a0f28d5a7c3fb7e803886eebb2bdc51e478e0
leomosley <leo.mosley2@gmail.com>
2024-10-16 19:37:50 +0100

    feat: update header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,23 +2,25 @@
 import React from 'react';
 import Link from 'next/link';
 import { HiOutlinePencilAlt } from 'react-icons/hi';
-import { LuFlower2, LuFlower } from 'react-icons/lu';
+import { LuFlower } from 'react-icons/lu';
 import { usePathname } from 'next/navigation';
 
 export function Header() {
   const pathname = usePathname();
   return !pathname.startsWith('/message') && (
-    <header className="fixed flex items-center w-full p-2 z-20 backdrop-blur-sm border-b border-b-muted">
-      <Link
-        className="w-9 h-9 md:h-10 md:w-10"
-        href="/"
-      ><LuFlower className="w-full h-full" />
-      </Link>
-      <Link
-        className="w-9 h-9 md:h-10 md:w-10 ml-auto"
-        href="/new"
-      ><HiOutlinePencilAlt className="w-full h-full" />
-      </Link>
+    <header className="fixed flex justify-center w-full p-2 z-20 backdrop-blur-xl border-b border-b-muted">
+      <div className="flex w-full md:max-w-6xl justify-between">
+        <Link
+          className="w-9 h-9 md:h-10 md:w-10"
+          href="/"
+        ><LuFlower className="w-full h-full" />
+        </Link>
+        <Link
+          className="w-9 h-9 md:h-10 md:w-10 ml-auto"
+          href="/new"
+        ><HiOutlinePencilAlt className="w-full h-full" />
+        </Link>
+      </div>
     </header>
   )
 }

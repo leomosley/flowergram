7bb0fb11d74ec2d13b27917513abe59a9f165794
leomosley <leo.mosley2@gmail.com>
2024-10-18 12:13:18 +0100

    feat(account): loading.tsx

---
+++ app/account/loadintg.tsx
@@ -0,0 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function AccountLoading() {
+  return (
+    <>
+      <Skeleton className="flex rouded-md h-36 p-4" />
+      <Skeleton className="flex rouded-md h-36 p-4" />
+      <Skeleton className="flex rouded-md h-36 p-4" />
+      <Skeleton className="flex rouded-md h-36 p-4" />
+      <Skeleton className="flex rouded-md h-36 p-4" />
+      <Skeleton className="flex rouded-md h-36 p-4" />
+      <Skeleton className="flex rouded-md h-36 p-4" />
+    </>
+  );
+}

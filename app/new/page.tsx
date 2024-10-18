9c8afbbd1c8f6f3bbf96e1e845d60c8e643e824d
leomosley <leo.mosley2@gmail.com>
2024-10-18 15:41:31 +0100

    fix(new): x padding

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -2,7 +2,7 @@ import { MessageForm } from "@/components/new/message-form";
 
 export default function New() {
   return (
-    <section className="flex h-screen py-12 justify-center items-center">
+    <section className="flex h-screen px-4 py-12 justify-center items-center">
       <MessageForm />
     </section>
   )
